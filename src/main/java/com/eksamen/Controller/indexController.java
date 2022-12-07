35d9faad512b8ce989683d7aa025adf5ada5581c
Flamme2 <flaviemahilyves@gmail.com>
2022-12-07 15:13:20 +0100

    html n stuff

--- src/main/java/com/eksamen/Controller/indexController.java
+++ src/main/java/com/eksamen/Controller/indexController.java
@@ -1,11 +1,32 @@
 package com.eksamen.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+//Flavie
 
+@Controller
 public class indexController {
 
     @GetMapping("/")
     public String index() {
         return "index";
     }
+    @GetMapping("/Admin")
+    public String Admin() {
+        return "Admin";
+    }
+
+    @GetMapping("/RegistrerLejeAftale")
+    public String RegistrerLejeAftale() {
+        return "RegistrerLejeAftale";
+    }
+
+    @GetMapping("/Udvikler")
+    public String Udvikler() {return "Udvikler";
+    }
+    @GetMapping("/Værksted")
+    public String Værksted() {return "Værksted";
+    }
 }
+
+
